9d77cb3848b400720d3e4710c3b7aca69c2ce3b5
serendipity-ljl <2235232654@qq.com>
2021-01-26 09:01:11 +0800

    week1:task 2

---
+++ week 1/src/Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int count=0,ge,shi,bai;
+        for(int i=100;i<1000;i++)
+        {
+             bai=i/100;
+             ge=i%10;
+             shi=(i-100*bai)/10;
+             if(ge*ge*ge+shi*shi*shi+bai*bai*bai==i)
+             {
+                 count++;
+             }
+        }
+        System.out.println("1000以内的水仙花数有"+count+"个");
+    }
+}
