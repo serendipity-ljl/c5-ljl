1f11ca217acaa90807336dc316081065001bf13f
serendipity-ljl <2235232654@qq.com>
2021-02-17 09:45:21 +0800

    week3:task 5

---
+++ week3/src/Task5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Task5 {
+      static  String[] usernames ={"张三","李四","王五"};
+      public static void main(String[] args) throws Exception {
+          Scanner in = new Scanner(System.in);
+          System.out.println("请输入用户名：");
+          String username = in.next();
+          CheckName(username);
+    }
+    public static void CheckName(String username) throws Exception {
+        for(String name:usernames)
+        {
+            if(name.equals(username))
+            {
+                try{
+                    throw new Exception("该用户已经被注册！");
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                return;
+            }
+        }
+        System.out.println("注册成功");
+    }
+}
